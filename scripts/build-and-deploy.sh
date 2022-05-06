708a960f60955320696c49283e44b312f6693fe0
Abel Salgado Romero <romeroab@vmware.com>
2022-05-06 23:08:29 +0200

    Automate deployment & update in k8s cluster

--- scripts/build-and-deploy.sh
+++ scripts/build-and-deploy.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 set -euo pipefail
 
-readonly PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. && pwd)"
-readonly WAVEFRONT_API_TOKEN="${1:?Wavefront API token must be set}"
-readonly DASHBOARD_LOCATION="${PROJECT_ROOT}/dashboards/wavefront-spring-cloud-gateway-for-kubernetes.json"
+readonly REGISTRY="${1:?Remote image registry domain must be set}"
 
-export_dashboard() {
-  curl 'https://vmware.wavefront.com/api/v2/dashboard/Spring-Cloud-Gateway-for-Kubernetes' \
-    --header "Authorization: Bearer ${WAVEFRONT_API_TOKEN}" |
-    jq .response -M >"${DASHBOARD_LOCATION}"
-}
+readonly PROJECT_NAME=$(gradle properties -q | grep "name:" | awk '{print $2}')
+readonly BUILD_VERSION=$(gradle properties -q | grep "version:" | awk '{print $2}')
 
 main() {
-  ./gradlew clean bootBuildImage
-  kubectl create
+  ./gradlew -project-prop registry="$REGISTRY" bootBuildImage
+  # Add timestamp to avoid node caching
+  local image="$REGISTRY/$PROJECT_NAME:$BUILD_VERSION-$(date +%s)"
+  docker tag "$REGISTRY/$PROJECT_NAME:$BUILD_VERSION" $image
+  docker push $image
+  kubectl create deploy $PROJECT_NAME --image $PROJECT_NAME -o json --dry-run=client | jq ".spec.template.spec.containers[0].image=\"$image\"" | kubectl apply -f -
 }
 
 main
