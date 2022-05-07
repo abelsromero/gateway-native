12e5fcf1ecf11a7de1370b63d7e2927370de8a9c
Abel Salgado Romero <abelromero@gmail.com>
2022-05-07 16:09:07 +0200

    Support KinD deployments

--- scripts/build-and-deploy.sh
+++ scripts/build-and-deploy.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
 set -euo pipefail
 
-readonly REGISTRY="${1:?Remote image registry domain must be set}"
-
+IS_KIND_CLUSTER="false" && [[ "$(kubectl config view -o json | jq -r '.["current-context"]')" =~ "kind-"* ]] && IS_KIND_CLUSTER="true"
+  
+if [[ "$IS_KIND_CLUSTER" == "false" ]]; then
+  readonly REGISTRY="${1:?Remote image registry domain must be set}"  
+else
+  readonly REGISTRY="local"
+fi
+  
 readonly PROJECT_NAME=$(gradle properties -q | grep "name:" | awk '{print $2}')
-readonly BUILD_VERSION=$(gradle properties -q | grep "version:" | awk '{print $2}')
+readonly PROJECT_VERSION=$(gradle properties -q | grep "version:" | awk '{print $2}')
+readonly IMAGE="$REGISTRY/$PROJECT_NAME:$PROJECT_VERSION-$(date +%Y%m%d%H%M%S)"
+
+build() {
+  ./gradlew --project-prop registry="$REGISTRY" bootBuildImage  
+}
+
+push() {
+   # Add timestamp to avoid node caching
+  docker tag "$REGISTRY/$PROJECT_NAME:$PROJECT_VERSION" $IMAGE
+      
+  if [[ "$IS_KIND_CLUSTER" == "true" ]]; then
+    kind load docker-image "$IMAGE"
+  else
+    docker push $IMAGE
+  fi
+}
+
+deploy() {
+  kubectl create deploy $PROJECT_NAME --image $PROJECT_NAME -o json --dry-run=client | jq ".spec.template.spec.containers[0].image=\"$IMAGE\"" | kubectl apply -f -
+}
 
 main() {
-  ./gradlew -project-prop registry="$REGISTRY" bootBuildImage
-  # Add timestamp to avoid node caching
-  local image="$REGISTRY/$PROJECT_NAME:$BUILD_VERSION-$(date +%s)"
-  docker tag "$REGISTRY/$PROJECT_NAME:$BUILD_VERSION" $image
-  docker push $image
-  kubectl create deploy $PROJECT_NAME --image $PROJECT_NAME -o json --dry-run=client | jq ".spec.template.spec.containers[0].image=\"$image\"" | kubectl apply -f -
+  build
+  push
+  deploy
 }
 
 main
